1c48d55bbf6aadb371ab1565d0f00b117f4fc908
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-10-09 16:06:52 -0600

    My server is set up and running

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = require("app");
+
+
+const port = 3000;
+
+
+
+
+
+app.listen(port, function() {
+    console.log("App is running on port: ", port);
+  });
